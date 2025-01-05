531ccee75aa7ca1eb7f3df9939ba27020508c02e
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-05 09:38:30 +0530

    add code

--- contextapp/src/App.jsx
+++ contextapp/src/App.jsx
@@ -43,7 +43,7 @@ useEffect(()=>{
 },[todos])
 
   return (
-    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo}} >
+    <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}} >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
