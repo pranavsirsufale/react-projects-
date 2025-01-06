d3744681af50ffa15ce2b97dcbceac3a02cab37d
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-06 21:10:39 +0530

    add jandog project  code

--- djangoface/myworld/facelogin/loginapp/views.py
+++ djangoface/myworld/facelogin/loginapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse(
+
+        '<h1> hello world <h1/>'
+    )
+
+
 def members(requrest):
     return HttpResponse('hello world')
