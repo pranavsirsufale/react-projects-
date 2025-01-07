7cb410e302e877287044c315c5615a032728f261
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 12:31:06 +0530

    add django backend code

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -19,27 +19,6 @@ export default function App() {
     <h1>
       hello guys there are so many of us 
     </h1>
-
-      {/* <Editor
-       
-        onInit={(_evt, editor) => editorRef.current = editor}
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button onClick={log}>Log editor content</button> */}
-    </>
+ </>
   );
 }
