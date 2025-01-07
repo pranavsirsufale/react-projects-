50a376b719adf5d4724ac8a0b7b4e3bc85789610
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 22:13:46 +0530

    add django backend code

--- megablog/src/components/card/PostCard.jsx
+++ megablog/src/components/card/PostCard.jsx
@@ -1,24 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-
-const PostCard = (
-    {
-        $id,
-        title,
-        featuredImage
-    }
-) => {
+const PostCard = ({ $id, title, featuredImage }) => {
   return (
-    <Link to={`/post/${$id}`} className="w-full bg-gray-50 rounded-xl p-4" >
-            <div className="w-full justify-center mb-4" >
-                <img src="" alt="" />
-                <h2>
-                    {title}
-                </h2>
-            </div>
-
+    <Link to={`/post/${$id}`} className="w-full bg-gray-50 rounded-xl p-4">
+      <div className="w-full justify-center mb-4">
+        <img src="" alt="" />
+        <h2>{title}</h2>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
