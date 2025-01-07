39ac5157d0fd38eb6b750bb5016a2080b10e8b82
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 18:06:11 +0530

    add django backend code

--- megablog/src/components/Header/Header.jsx
+++ megablog/src/components/Header/Header.jsx
@@ -1,18 +1,87 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Container from '../container/Container'
-import { Link , useNavigate } from "react-router-dom";
+import { Link , NavLink, useNavigate } from "react-router-dom";
 import { Logo, LogoutBtn} from '../../components'
 
 
 
 const Header = () => {
-  const { status , userData } = useSelector((state)=>state.auth)
+  const navigate = useNavigate()
+  const authStatus = useSelector((state)=>state.auth.status)
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ]
+
 
   return (
     <header className='py-3 shadow bg-gray-500'>
     <Container>
-      <nav className='flex'>
+
+      <nav className="flex" > 
+        <div className="mr-4" >
+          <Link to='/' >
+          <Logo/>
+          </Link>
+        </div>
+
+        <div>
+
+          <ul className="flex ml-auto" >
+
+            
+
+           
+
+            {
+              navItems.map((eachLink , index)=>(
+                eachLink.active ? 
+                <li key={index} >
+                 <button onClick={()=>navigate(eachLink.slug)} 
+                  className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                  {eachLink.name}
+                 </button>
+                </li> :
+                null
+              ))
+            }
+          </ul>
+
+
+        </div>
+
+
+      </nav>
+
+
+
+      {/* <nav className='flex'>
         <div className='mr-4'>
           <Link to='/'>
             <Logo width='70px'   />
@@ -30,13 +99,13 @@ const Header = () => {
             </li>
           ) : null
           )}
-          { status && (
+          { authStatus && (
             <li>
               <LogoutBtn />
             </li>
           )}
         </ul>
-      </nav>
+      </nav> */}
       </Container>
   </header>
   )
