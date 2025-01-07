d4981f2ca0cb723723fa6981d307c5129717436d
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 10:33:04 +0530

    add django backend code

--- djangoface/myworld/facelogin/loginapp/views.py
+++ djangoface/myworld/facelogin/loginapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def home(request):
     homepage = loader.get_template('register.html')
+    members = Member.objects.all().values()
+    constext = {
+        'mymembers':members,
+    }
     return HttpResponse(
-        homepage.render()
+        homepage.render(constext,request)
     )
 
 
