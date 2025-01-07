17262f886cca838bb860e55015423a83086e039a
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 14:01:24 +0530

    add django backend code

--- megablog/src/appwrite/auth.js
+++ megablog/src/appwrite/auth.js
@@ -1,22 +1,54 @@
 import { Client, Account, ID } from 'appwrite'
 import {appwrite_api_endpoint,appwrite_project_id} from '../conf/confVariables.js'
 
+export class AuthService{
+    client = new Client()
+    account ;
 
-//! client creation 
-const client = new Client()
-.setEndpoint(appwrite_api_endpoint)
-.setProject(appwrite_project_id)
+    constructor(){
+        this.client
+        .setEndpoint(appwrite_api_endpoint)
+        .setProject(appwrite_project_id);
+
+        this.account = new Account(this.client)
+    }
+
+    //! account creation service
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            )
+
+            if(userAccount){
+                return this.login(email,password)
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password)
+
+        } catch (error) {
+            throw error
+        }
+
+    }
 
 
-const account = new Account(client)
+}
+const authService = new AuthService();
 
-//! user account creation
-const user = await account.create(
-    ID.unique(),
-    
-
-)
-
+export default authService;
 
 
 
