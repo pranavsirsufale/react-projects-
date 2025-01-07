3f1e1e8c2b91349b5b6a83e51cc237c0589a8377
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 16:03:08 +0530

    add django backend code

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -1,39 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { useDispatch } from 'react-redux'
-import authService from './appwrite/auth'
-import { login ,logout } from './store/slice/authSlice'
+import { useEffect, useRef, useState } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/slice/authSlice";
 
 export default function App() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    authService.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-
-
-
-    })
-    .finally(()=>{
-      setLoading(false)
-    })
-  },[])
-
-
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <>
-    <h1>
-      hello guys there are so many of us 
-    </h1>
-   
-   </>
+      <h1>hello guys there are so many of us</h1>
+    </>
   );
 }
