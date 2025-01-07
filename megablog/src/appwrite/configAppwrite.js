41b8be8ec2bed69d5b44a4994107b88277c1e014
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 15:18:31 +0530

    add django backend code

--- megablog/src/appwrite/configAppwrite.js
+++ megablog/src/appwrite/configAppwrite.js
@@ -1,82 +1,115 @@
-import { appwrite_api_endpoint, appwrite_collection_id, appwrite_db_id, appwrite_project_id } from '../conf/confVariables.js'
-import { Client, ID,Databases,Storage,Query  } from 'appwrite'
+import {
+  appwrite_api_endpoint,
+  appwrite_bucket_id,
+  appwrite_collection_id,
+  appwrite_db_id,
+  appwrite_project_id,
+} from "../conf/confVariables.js";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service{
-    client = new Client()
-    databases ;
-    storage;
-    
-    constructor(){
-        //! client
-        this.client
-        .setEndpoint(appwrite_api_endpoint)
-        .setProject(appwrite_project_id);
+export class Service {
+  client = new Client();
+  databases;
+  storage;
 
-        //! databases
-        this.databases = new Databases(this.client)
+  constructor() {
+    //! client
+    this.client
+      .setEndpoint(appwrite_api_endpoint)
+      .setProject(appwrite_project_id);
 
-        //! bucket
-        this.storage = new Storage(this.client)
-    }
+    //! databases
+    this.databases = new Databases(this.client);
 
-    async createPost({title,slug,content,featuredImage,status,userId}){
-        try {
-            return await this.databases.createDocument(appwrite_db_id,appwrite_collection_id,slug,{
-                title,
-                content,
-                featuredImage,
-                status,
-                userId
-            })
-        } catch (error) {
-            throw error
+    //! bucket
+    this.storage = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        appwrite_db_id,
+        appwrite_collection_id,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
         }
+      );
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async updatePost(slug,{title,content,featuredImage,status}){
-        try {
-            return await this.databases.updateDocument(appwrite_db_id,appwrite_collection_id,slug,
-                {
-                    title,
-                    content,
-                    featuredImage,
-                    status
-                }
-            )
-        } catch (error) {
-            throw error
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        appwrite_db_id,
+        appwrite_collection_id,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
         }
+      );
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async deletePost(slug){
-        try {
-            await this.databases.deleteDocument(appwrite_db_id,appwrite_collection_id,slug)
-            return true
-        } catch (error) {
-            throw error
-        }
-
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteDocument(
+        appwrite_db_id,
+        appwrite_collection_id,
+        slug
+      );
+      return true;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getPost(slug){
-        try {
-            return await this.databases.getDocument(appwrite_db_id,appwrite_collection_id,slug)
-        } catch (error) {
-            throw error
-        }
+  async getPost(slug) {
+    try {
+      return await this.databases.getDocument(
+        appwrite_db_id,
+        appwrite_collection_id,
+        slug
+      );
+    } catch (error) {
+      throw error;
     }
+  }
 
-    async getPosts(queries=[Query.equal('status','active')]){
-        try {
-            return await this.databases.listDocuments(appwrite_db_id,appwrite_collection_id,queries)
-        } catch (error) {
-            throw error
-        }
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        appwrite_db_id,
+        appwrite_collection_id,
+        queries
+      );
+    } catch (error) {
+      throw error;
     }
+  }
 
+  //! file upload service
 
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(appwrite_bucket_id, ID.unique, file);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
-const service = new Service()
+const service = new Service();
 
 export default service;
