50e96420397e5aabd4df4c7c9a480a7a11c0014f
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 14:08:38 +0530

    add django backend code

--- megablog/src/appwrite/auth.js
+++ megablog/src/appwrite/auth.js
@@ -44,6 +44,40 @@ export class AuthService{
 
     }
 
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error;
+        } 
+
+
+        return null;
+
+        
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+
+
+
+
+        return null
+
+
+
+
+    }
+
+
+
+
+
 
 }
 const authService = new AuthService();
