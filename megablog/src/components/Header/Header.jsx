324b385434e91bc4917aefd3a87897bc504f2847
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 17:44:41 +0530

    add django backend code

--- megablog/src/components/Header/Header.jsx
+++ megablog/src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Container from '../container/Container'
+import { Link , useNavigate } from "react-router-dom";
+import { Logo, LogoutBtn} from '../../components'
+
+
 
 const Header = () => {
-  return <div>Header</div>;
+  const { status , userData } = useSelector((state)=>state.auth)
+
+  return (
+    <header className='py-3 shadow bg-gray-500'>
+    <Container>
+      <nav className='flex'>
+        <div className='mr-4'>
+          <Link to='/'>
+            <Logo width='70px'   />
+
+            </Link>
+        </div>
+        <ul className='flex ml-auto'>
+          {navItems.map((item) => 
+          item.active ? (
+            <li key={item.name}>
+              <button
+              onClick={() => navigate(item.slug)}
+              className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+              >{item.name}</button>
+            </li>
+          ) : null
+          )}
+          { status && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+        </ul>
+      </nav>
+      </Container>
+  </header>
+  )
 };
 
 export default Header;
--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -1,3 +1,6 @@
 import Header from "./Header/Header";
 import Footer from './Footer/Footer'
-export { Header,Footer}
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+// import Container from './container/Container'
+export { Header,Footer ,Logo ,LogoutBtn , }
