ec31bc693821ee0cc807d3b9930a1700ea7a9780
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-07 08:43:57 +0530

    add jandog project  code

--- djangoface/myworld/facelogin/loginapp/models.py
+++ djangoface/myworld/facelogin/loginapp/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Member(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
-    phone = models.IntegerField()
-    joined_date = models.DateField()
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
