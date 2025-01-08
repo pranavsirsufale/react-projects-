4bd43d7869c3d6c492ff7f654a1004410476a66c
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-08 19:39:18 +0530

    add django backend code

--- megablog/src/components/card/PostCard.jsx
+++ megablog/src/components/card/PostCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import service from "../../appwrite/configAppwrite";
 
 const PostCard = ({ 
     $id,
@@ -10,9 +11,13 @@ const PostCard = ({
       <div  className="w-full bg-gray-100 rounded-xl p-4" >
 
       <div className="w-full justify-center mb-4">
-        <img src="" alt="" />
+        <img src={`${service.getFilePreview(featuredImage)}`} alt={title} 
+        className="rounded-xl"
+        />
       </div>
-        <h2>{title}</h2>
+        <h2
+        className="text-xl"
+        >{title}</h2>
       </div>
     </Link>
   );
