c08a995bb32bb059f654c0f795b0f799e96d380e
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-08 20:00:18 +0530

    add django backend code

--- megablog/src/components/userauth/Login.jsx
+++ megablog/src/components/userauth/Login.jsx
@@ -1,51 +1,44 @@
-import { Link,useNavigate } from 'react-router-dom'
-import { login as authLogin} from '../../store/slice/authSlice'
-import { Button , Input , Logo } from '../index'
-import authService from '../../appwrite/auth'
-import { useSelector , useDispatch } from 'react-redux'
-import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../../store/slice/authSlice";
+import { Button, Input, Logo } from "../index";
+import authService from "../../appwrite/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Login = () => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const { register, handleSubmit } = useForm()
-    const [error, setError ] = useState('')
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
 
-    const login = async(data) => {
-        setError('')
-        try {
-            const session = await authService.login(data)
-            if( session){
-                const userData = await authService.getCurrentUser()
-                if(userData){
-                    dispatch(authLogin)
-                    navigate('/')
-                }
-            }
-
-        } catch (error) {
-            setError(error.message)
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) {
+          dispatch(authLogin);
+          navigate("/");
         }
+      }
+    } catch (error) {
+      setError(error.message);
     }
+  };
   return (
-    <div className='flex items-center justify-center w-full' >
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`} >
-
-        <div className='mb-2 flex justify-center' >
-
-            <span className='inline-block w-full max-w-[100px]' >
-                <Logo width='100%' /> 
-            </span>
-
+    <div className="flex items-center justify-center w-full">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
         </div>
-
-
-
-        </div>
-
-
+      </div>
     </div>
-  )
-}
-export default Login
+  );
+};
+export default Login;
