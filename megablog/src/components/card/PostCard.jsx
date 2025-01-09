f7586d7b828da6c98d90b54786d8dde541179c56
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 17:32:56 +0530

    add functionality

--- megablog/src/components/card/PostCard.jsx
+++ megablog/src/components/card/PostCard.jsx
@@ -1,34 +1,22 @@
 import { Link } from "react-router-dom";
 import appWriteService from "../../appwrite/configAppwrite";
 
-const PostCard = (
-  {
-    $id,
-    title,
-    featuredImage
-  }
-) => {
+const PostCard = ({ $id, title, featuredImage }) => {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={appWriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl"
+          />
 
-return(
-  <Link to={`/post/${$id}`} >
-    <div className="w-full bg-gray-100 rounded-xl p-4" >
-
-      <div className="w-full justify-center mb-4" >
-
-        <img src={appWriteService.getFilePreview(featuredImage)} alt={title}  className="rounded-xl" />
-
-    <h2 className="text-xl font-bold" >
-
-      {title}
-    </h2>
-        
+          <h2 className="text-xl font-bold">{title}</h2>
+        </div>
       </div>
-
-    </div>
-  
-  </Link>
-)
-
-}
+    </Link>
+  );
+};
 
 export default PostCard;
