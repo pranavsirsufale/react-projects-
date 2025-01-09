00cb0b8a442088a94165d395d03d1a97654d0329
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 22:53:10 +0530

    add functionality

--- megablog/src/components/postform/PostForm.jsx
+++ megablog/src/components/postform/PostForm.jsx
@@ -2,9 +2,27 @@ import React,{useCallback} from 'react'
 import { useForm } from 'react-hook-form'
 import { Button, Input, Select, RTE} from '../index'
 import appWriteService from '../../appwrite/configAppwrite'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 
-const PostForm = () => {
+const PostForm = ({post}) => {
+    const { register, handleSubmit,watch,setValue,control,getValues} = useForm({
+        defaultValues : {
+            title : post?.title || '',
+            slug : post?.slug || '',
+            content : post?.content || '',
+            status : post?.status || 'active',
+
+        }
+    })
+
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.auth.userData)
+
+
+
+
   return (
     <div>
 
