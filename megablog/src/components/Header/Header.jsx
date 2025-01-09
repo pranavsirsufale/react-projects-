0c5cae729f0f60203f54d8c7e2e51369da086b81
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 16:43:56 +0530

    add functionality

--- megablog/src/components/Header/Header.jsx
+++ megablog/src/components/Header/Header.jsx
@@ -1,95 +1,76 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Container from '../container/Container'
-import { Link , NavLink, useNavigate } from "react-router-dom";
-import { Logo, LogoutBtn} from '../../components'
-
-
+import Container from "../container/Container";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Logo, LogoutBtn } from "../../components";
 
 const Header = () => {
-  const navigate = useNavigate()
-  const authStatus = useSelector((state)=>state.auth.status)
+  const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
 
   const navItems = [
     {
-      name: 'Home',
+      name: "Home",
       slug: "/",
-      active: true
-    }, 
+      active: true,
+    },
     {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-  },
-  {
+    },
+    {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-  },
-  {
+    },
+    {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-  },
-  ]
-
+    },
+  ];
 
   return (
-    <header className='py-3 shadow bg-gray-500'>
-    <Container>
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo />
+            </Link>
+          </div>
 
-      <nav className="flex" > 
-        <div className="mr-4" >
-          <Link to='/' >
-          <Logo/>
-          </Link>
-        </div>
+          <div>
+            <ul className="flex ml-auto">
+              {navItems.map((eachLink, index) =>
+                eachLink.active ? (
+                  <li key={index}>
+                    <button
+                      onClick={() => navigate(eachLink.slug)}
+                      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    >
+                      {eachLink.name}
+                    </button>
+                  </li>
+                ) : null
+              )}
+            </ul>
+          </div>
 
-        <div>
+          {authStatus && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+        </nav>
 
-          <ul className="flex ml-auto" >
-
-            
-
-           
-
-            {
-              navItems.map((eachLink , index)=>(
-                eachLink.active ? 
-                <li key={index} >
-                 <button onClick={()=>navigate(eachLink.slug)} 
-                  className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
-                  >
-                  {eachLink.name}
-                 </button>
-                </li> :
-                null
-              ))
-            }
-          </ul>
-
-
-        </div>
-
-            {
-              authStatus && (
-                <li>
-                  <LogoutBtn/>
-                </li>
-              )
-             
-            }
-
-      </nav>
-
-
-
-      {/* <nav className='flex'>
+        {/* <nav className='flex'>
         <div className='mr-4'>
           <Link to='/'>
             <Logo width='70px'   />
@@ -115,8 +96,8 @@ const Header = () => {
         </ul>
       </nav> */}
       </Container>
-  </header>
-  )
+    </header>
+  );
 };
 
 export default Header;
