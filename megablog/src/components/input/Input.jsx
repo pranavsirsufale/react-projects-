dafb26ff05198e89e3d05c984218997ee496295f
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 17:06:48 +0530

    add functionality

--- megablog/src/components/input/Input.jsx
+++ megablog/src/components/input/Input.jsx
@@ -15,7 +15,7 @@ const Input = ({ label, type = "text", className = "", ...props }, ref) => {
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
         {...props}
-        id="id"
+        id={id}
       />
     </div>
   );
