7a14dad54676ed0ac117daa1b2a1b06b251c0c8b
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 17:05:01 +0530

    add functionality

--- megablog/src/components/input/Input.jsx
+++ megablog/src/components/input/Input.jsx
@@ -1,28 +1,49 @@
 import React, { forwardRef, useId } from "react";
 
-const Input = forwardRef(function (
-  { label, type = "text", className = "", ...props },
-  ref
-) {
-  const id = useId();
+const Input = ({
+  label ,
+  type = 'text',
+  className = '',
+  ...props
+},
+ref
+) => {
 
+
+  const id = useId()
   return (
-    <div className="w-full">
-      {label && (
-        <label className="inline-block mb-1 pl-1" htmlFor={id}>
-          {label}
-        </label>
-      )}
+   <div className="w-full" >
+    {
+      label && 
+      <label
+      className="inline-block mb-1 pl-1"
+      htmlFor={id}
+      >
+{
+  label
+}
 
-      <input
+
+      </label>
+
+      
+    }
+
+
+<input
         type={type}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
         {...props}
         id="id"
       />
-    </div>
-  );
-});
 
-export default Input;
+   </div>
+
+
+
+  )
+
+}
+
+export default forwardRef(Input);
