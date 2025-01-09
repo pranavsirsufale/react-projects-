a810c1f892e7b767db0f07cfdc97cdd6e0c887bf
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-09 19:01:59 +0530

    add functionality

--- megablog/src/components/userauth/Signup.jsx
+++ megablog/src/components/userauth/Signup.jsx
@@ -1,51 +1,45 @@
-import React,{useState} from 'react'
-import authService from '../../appwrite/auth'
-import {Login, Logo} from '../index'
-import { login as loginStore } from '../../store/slice/authSlice'
-import { useDispatch } from 'react-redux'
-import { set, useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-
-
-
+import React, { useState } from "react";
+import authService from "../../appwrite/auth";
+import { Input, Login, Logo } from "../index";
+import { login as loginStore } from "../../store/slice/authSlice";
+import { useDispatch } from "react-redux";
+import { set, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const [error , setError ] = useState('')
+  const [error, setError] = useState("");
 
-    const { register, handleSubmit} = useForm()
-    
+  const { register, handleSubmit } = useForm();
 
-    const signUp = async( data) => {
-        setError('')
-        try {
-            const registerdUser = await authService.createAccount(data);
-            if(registerdUser){
-                const userData = await authService.getCurrentUser(data) 
+  const signUp = async (data) => {
+    setError("");
+    try {
+      const registerdUser = await authService.createAccount(data);
+      if (registerdUser) {
+        const userData = await authService.getCurrentUser(data);
 
-                if(userData){
-                    dispatch(loginStore(userData))
-                    navigate('/')
-                    
-                }
-            }
-        } catch (error) {
-            setError(error.message)
+        if (userData) {
+          dispatch(loginStore(userData));
+          navigate("/");
         }
+      }
+    } catch (error) {
+      setError(error.message);
     }
+  };
 
   return (
-    <div className='flex items-center justify-center' >
-        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`} >
-
-        <div className='mb-2 flex justify-center' >
-
-
-        <span className='inline-block w-full max-w-[100px]' >
-         <Logo width='100%' /> 
-        </span>
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
         </div>
 
         <h2 className="text-center text-2xl font-bold">
@@ -62,20 +56,32 @@ const Signup = () => {
           </Link>
         </p>
 
+        {error && <p className="text-red-600 mt-8">{error}</p>}
+ 
 
-        {
-            error && <p className='text-red-600 mt-8' > 
-                {error}
-            </p>
-        }
+        <form onSubmit={handleSubmit(signUp())} >
+            <div onSubmit='space-y-5' >
+
+                    <Input
+                    label='Full Name: '
+                    placeholder='Enter Your Name'
+                    {...register('name',{
+                        required : true,
+                        
+                    } )}
+                    
+                    />
+
+            </div>
+
+            
+        </form>
 
 
 
-        </div>
-        
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
