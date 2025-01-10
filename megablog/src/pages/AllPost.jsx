5529b6bf366c59ec05ad8ff980206c6118dddb3b
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-10 20:41:09 +0530

    add functionality

--- megablog/src/pages/AllPost.jsx
+++ megablog/src/pages/AllPost.jsx
@@ -1,46 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import appWriteService from '../appwrite/configAppwrite'
-import Container from '../components/container/Container'
-import { PostCard } from '../components'
+import React, { useEffect, useState } from "react";
+import appWriteService from "../appwrite/configAppwrite";
+import Container from "../components/container/Container";
+import { PostCard } from "../components";
 
-const AllPost = ({ }) => {
-  const [posts, setPosts] = useState([])
-  
-  useEffect(()=> {
-    appWriteService.getPosts([]).then((posts)=> 
-    
-    {
-      if(posts){
-        setPosts(posts.documents)
+const AllPost = ({}) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    appWriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
       }
-    })
-    
-  },[])
-
+    });
+  }, []);
 
   return (
-    <div className='w-full py-8' >
-        <Container>
-
-        <div className='flex flex-wrap' >
-
-          {
-            posts.map((post)=> (
-              <div key={post.$id} className='p-2 w-1/4'  >
-                  <PostCard
-                post={post}
-                  />
-              </div>
-            ))
-          }
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard post={post} />
+            </div>
+          ))}
         </div>
-
-
-        </Container>
-
-        
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default AllPost
+export default AllPost;
