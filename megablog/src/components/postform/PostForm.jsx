ea5c739afaf8335ba59311f37a0743b1bbe90bee
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-10 06:46:52 +0530

    add functionality

--- megablog/src/components/postform/PostForm.jsx
+++ megablog/src/components/postform/PostForm.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { Button, Input, Select, RTE } from "../index";
 import appWriteService from "../../appwrite/configAppwrite";
@@ -69,7 +69,71 @@ const PostForm = ({ post }) => {
 
 
 
-  return <div></div>;
-};
+  useEffect(()=>{
+    const subscription = watch((value,{name})=>{
+        if(name ==='title'){
+            setValue('slug' , slugTransform(value.title,{
+                shouldValidate : true
+            }))
+        }
+    })
+
+
+    return ()=>{
+        subscription.unsubscribe()
+    }
+
+
+  },[slugTransform,watch,setValue])
+
+
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap" >
+            <div className="w-2/3 px-2" >
+                <Input
+                    label='Title :'
+                    placeholder='Title'
+                    className='mb-4'
+                    {
+                        ...register('title',{
+                            required: true
+                        })
+                    }
+                />
+
+
+                <Input
+
+                />
+
+                </div>
+
+                <div className="w-1/3 px-2" >
+
+                        <Input
+                        label='featured Image :'
+                        type='file'
+                        className='mb-4'
+                        accept='image/png, image/jpg, image/jpeg, image/gif'
+
+                        {
+                            ...register('image',{
+                                required : !post
+                            })
+                        }
+
+                        />
+
+                        {
+                            post && ()
+                        }
+
+            </div>
+
+    </form>
+  )
+
+}
 
 export default PostForm;
