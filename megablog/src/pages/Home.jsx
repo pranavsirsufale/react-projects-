df9278f9687f364958769883915ff5d5796e254c
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-10 20:36:36 +0530

    add functionality

---
+++ megablog/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import Container from '../components/container/Container'
+import { PostCard } from '../components'
+import appWriteService from '../appwrite/configAppwrite'
+
+
+
+const Home = () => {
+    
+    const [ posts, setPosts] = useState([])
+
+    useEffect(()=>{
+    appWriteService.getPosts()
+        .then((posts)=>{
+            if(posts){
+                setPosts(posts)
+            }
+        })
+    }
+
+)
+
+
+  if (posts.length === 0){
+    return (
+        <div className='w-full py-8 mt-4 text-center'
+         >
+
+            <Container>
+                <div className='flex flex-wrap' >
+
+                    <div className='p-2 w-full' >
+
+                        <h1 className='text-2xl font-bold hover:text-gray-500' >
+
+                            Login to read posts 
+                        </h1>
+
+                    </div>
+                    
+                    
+                </div>    
+                
+            </Container>  
+
+        </div>
+    )
+  }
+}
+
+export default Home
