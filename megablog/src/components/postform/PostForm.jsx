81227b9c527a49dbe8419381f060a967e6e1de18
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-10 06:16:03 +0530

    add functionality

--- megablog/src/components/postform/PostForm.jsx
+++ megablog/src/components/postform/PostForm.jsx
@@ -6,8 +6,40 @@ import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 
-const PostForm = () => {
-    
+const PostForm = ({post}) => {
+    const {register, handleSubmit,watch,setValue, control, getValues} = useForm({
+        defaultValues : {
+            title : post?.title || '',
+            slug : post?.slug || '',
+            content : post?.content || '',
+            status : post?.status || ''
+        }
+    })
+
+    const navigate = useNavigate()
+    const userData = useSelector(state=> state.auth.userData)
+
+
+    const submit = async ( data) => {
+        if(post){
+            const file = data.image[0] ? await appWriteService.uploadFile(data.image[0]) :null;
+
+            if(file){
+                await appWriteService.deleteFile(post.featuredImage)
+            }
+
+            const dbPost = await appWriteService.updatePost(post.$id , {
+                ...data,
+                featuredImage : file ? file.$id : undefined,
+            })
+
+            if(dbPost){
+                navigate(`/post/${dbPost.$id}`)
+            }
+        } else {
+            
+        }
+    }
 
 
 
