304bf29245eca69d915148863a0c49297c946f0a
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-10 20:26:36 +0530

    add functionality

---
+++ megablog/src/pages/EditPost.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+import Container from '../components/container/Container'
+import { PostForm } from '../components'
+import appWriteService from '../appwrite/configAppwrite'
+import { useNavigate, useParams } from 'react-router-dom'
+
+
+const EditPost = () => {
+
+    const [post,setPost] = useState(null)
+
+    const { slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(()=>
+    {
+        if(slug){
+            appWriteService.getPost(slug)
+            .then((post)=> {
+                if(post){
+                    setPost(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+    },[slug,navigate])
+
+  return  post ?  (
+    <div className='py-8' >
+        <Container>
+
+            <PostForm 
+                post={post}
+            /> 
+        </Container>
+
+    </div>
+  ) : <h1> No Post Found </h1>
+}
+
+export default EditPost
+
+
+
+
