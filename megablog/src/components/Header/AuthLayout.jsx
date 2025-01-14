664127980fbc545adfb776aea3051c69b4922609
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-14 09:19:54 +0530

    add functionality

--- megablog/src/components/Header/AuthLayout.jsx
+++ megablog/src/components/Header/AuthLayout.jsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import {useNavigate} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-export default function Protected({children,authentication=true}){
-
-    const navigate = useNavigate()
-    const [loader,setLoader] = useState(true)
-    const authStatus = useSelector((state)=>state.auth.status)
-
-
-    useEffect(()=>{
-        if (authentication && authStatus !== authentication) { 
-
-            console.log(authentication)
-            console.log(authStatus)
-            navigate('/login')
-        } else if(!authentication && authStatus !== authentication) {
-            navigate('/')
-        }
-
-        setLoader(false)
-
-    },[authStatus,navigate,authentication])
-
-  return loader ? <h1> Loading... </h1> : <> {children} </>
+export default function Protected({ children, authentication = true }) {
+  //! authentication true = can visit the route without authentication
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+  return loader ? <h1> Loading... </h1> : <> {children} </>;
 }
