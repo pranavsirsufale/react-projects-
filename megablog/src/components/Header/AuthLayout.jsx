2dfc8bd4b74b338080d595ce3ff83ef5e04b4562
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-14 10:28:14 +0530

    add functionality

--- megablog/src/components/Header/AuthLayout.jsx
+++ megablog/src/components/Header/AuthLayout.jsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Protected({ children, authentication = true }) {
   //! authentication true = can visit the route without authentication
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
+
+  const route = useLocation()
+
+console.log(authStatus)
+
+  
+
   console.log(authStatus)
   useEffect(() => {
     //
     if (authentication && authStatus !== authentication) {
       navigate("/login");
-    } 
-    // else if (!authentication && authStatus !== authentication) {
-    //   // navigate("/");
-    // }
+    } else if( authStatus && route.pathname === '/login') {
+        navigate('/')
+    } else if (authStatus && route.pathname === '/signup'){
+      navigate('/')
+    }
+    
     setLoader(false);
   }, [authStatus, navigate, authentication]);
   return loader ? <h1> Loading... </h1> : <> {children} </>;
--- megablog/src/components/Header/Header.jsx
+++ megablog/src/components/Header/Header.jsx
@@ -1,143 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Container from "../container/Container";
-import { Link, NavLink, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate,  useLocation } from "react-router-dom";
 import { Logo, LogoutBtn } from "../../components";
 
 const Header = () => {
   const navigate = useNavigate();
-  const status = useSelector((state) => state.auth.status);
-  
-  const [ authStatus , setAuthStatus] = useState(status)
-
-  const [navItems, setNavItems ] = useState([
-    {
-      name: "Home",
-      slug: "/",
-      active: true,
-    },
-    {
-      name: "Login",
-      slug: "/login",
-      active: !authStatus,
-    },
-    {
-      name: "Signup",
-      slug: "/signup",
-      active: !authStatus,
-    },
-    {
-      name: "All Posts",
-      slug: "/all-posts",
-      active: authStatus,
-    },
-    {
-      name: "Add Post",
-      slug: "/add-post",
-      active: authStatus,
-    },
-  ])
+  const authStatus = useSelector((state) => state.auth.status);
 
   // useEffect(()=>{
-    
+  //   authStatus
   // },[authStatus])
 
-
-
-
- useEffect(()=>{
-  // const navItems = [
-  //   {
-  //     name: "Home",
-  //     slug: "/",
-  //     active: true,
-  //   },
-  //   {
-  //     name: "Login",
-  //     slug: "/login",
-  //     active: !authStatus,
-  //   },
-  //   {
-  //     name: "Signup",
-  //     slug: "/signup",
-  //     active: !authStatus,
-  //   },
-  //   {
-  //     name: "All Posts",
-  //     slug: "/all-posts",
-  //     active: authStatus,
-  //   },
-  //   {
-  //     name: "Add Post",
-  //     slug: "/add-post",
-  //     active: authStatus,
-  //   },
-  // ];
-  setNavItems([
+  
+  const navItems = [
     {
-      name: "Home",
+      name: 'Home',
       slug: "/",
-      active: true,
-    },
+      active: true
+    }, 
     {
       name: "Login",
       slug: "/login",
       active: !authStatus,
-    },
-    {
+  },
+  {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
-    },
-    {
+  },
+  {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
-    },
-    {
+  },
+  {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
-    },
-  ])
-},[authStatus,navigate,navItems])
+  },
+  ]
 
   return (
-    <header className="py-3 shadow bg-gray-500">
+     <header className='py-3 shadow bg-gray-500'>
       <Container>
-        <nav className="flex">
-          <div className="mr-4">
-            <Link to="/">
-              <Logo />
-            </Link>
-          </div>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'   />
 
-          <div>
-            <ul className="flex ml-auto">
-              {navItems.map((eachLink, index) =>
-                eachLink.active ? (
-                  <li key={index}>
-                    <button
-                      onClick={() => navigate(eachLink.slug)}
-                      className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
-                    >
-                      {eachLink.name}
-                    </button>
-                  </li>
-                ) : null
-              )}
-            </ul>
+              </Link>
           </div>
-
-          {authStatus && (
-            <li>
-              <LogoutBtn />
-            </li>
-          )}
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
         </nav>
-
-  
-      </Container>
+        </Container>
     </header>
   );
 };
