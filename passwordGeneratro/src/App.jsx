6b4453515f7093b38b30ce9ed59d8c88baa23c59
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-18 14:14:53 +0530

    add Js logic

--- passwordGeneratro/src/App.jsx
+++ passwordGeneratro/src/App.jsx
@@ -1,94 +1,72 @@
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
-
-  const [len, setLen] = useState(8)
-
-  let smtext = 'abcdefghijklmnopqrstuvwxyz'
-  smtext += 'abcdefghijklmnopqrstuvwxyz'.toUpperCase()
-  smtext += '~!@#~`$%^&*^()-_' // 68
-  smtext += '0123456789' // 78
-  const max = smtext.length-1 ; // 78 - 0 + 
-
-  // console.log(Math.round((Math.random())* (max)) ) 
-
-  console.log(len)
+  const [len, setLen] = useState(8);
+  const [password, setPassword] = useState("");
 
 
-const [password , setPassword ] = useState('')
+  //? The string from the password to take 
+  let smtext = "abcdefghijklmnopqrstuvwxyz";
+  smtext += smtext.toUpperCase() + "~!@#~`$%^&*^()-_" + "0123456789";
+  const max = smtext.length - 1; 
 
- 
-
- 
-
+  //? to copy code on Clipboard
   const copyCode = (e) => {
     navigator.clipboard.writeText(e.target.value);
-    
-    console.log(e)
+    e.target.select();
+    alert(`The Code is Copied : ${e.target.value}`);
+  };
 
-    e.target.select()
-    
-    alert(`The Code is Copied : ${e.target.value}`)
-    
-  }
-
-  // const passwordGenerator = () => {
-    
-  // }
-
-  const passwordGenerator = useCallback(()=>{
-    let curr = ''
-    for(let i = 0 ; i < len ; i++){
-      curr += smtext[Math.round((Math.random())* (max))]
+  //? To generate random password with memoization
+  const passwordGenerator = useCallback(() => {
+    let curr = "";
+    for (let i = 0; i < len; i++) {
+      curr += smtext[Math.round(Math.random() * max)];
     }
 
-    setPassword(curr)
-
-  },[len,password])
-
+    setPassword(curr);
+  }, [len, password]);
 
 
 
-  useEffect(()=>{
-      passwordGenerator()
-   
-  
-  },[len])
-
-
+  useEffect(() => {
+    passwordGenerator();
+  }, [len]);
 
   return (
     <>
+      <div className="container">
+        <div>
+          <input
+            name="len"
+            id="len"
+            type="range"
+            max={16}
+            min={7}
+            value={len}
+            onChange={(e) => setLen(e.target.value)}
+          />
+          <input
+            type="text"
+            readOnly
+            value={password}
+            onClick={(e) => copyCode(e)}
+            style={{
+              textAlign: "center",
+              fontSize: "1.3rem",
+              backgroundColor: "#124",
+              border: "2px solid green",
+              borderRadius: "20px",
+            }}
+          />
+        </div>
 
-    <div className="container">
-
-    <div>
-
-    <input name="len" id="len" type="range" max={16} min={7}  value={len} onChange={(e)=>setLen(e.target.value)}
-    
-    />
-      <input type="text" readOnly value={password} 
-
-      onClick={(e)=>copyCode(e)}
-      
-      style={{textAlign:"center", fontSize : '1.3rem', backgroundColor: '#124',  border:"2px solid green" , borderRadius: '20px'}}
-      />
-    </div>
-
-    <div>
-    <label htmlFor="len"> {len} </label>
-
-    </div>
-
-    </div>
-
-
-
-    <h1>
-      Hii there's Pranav Sirsufale 
-    </h1>
+        <div>
+          <label htmlFor="len"> {len} </label>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
