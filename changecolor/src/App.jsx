c21276b31387f3efd1d8744cf8bf78723962aed1
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-18 08:45:37 +0530

    add React proejct

---
+++ changecolor/src/App.jsx
@@ -0,0 +1,30 @@
+import Buttons from "./components/Buttons"
+
+
+function App() {
+
+  return (
+    <div className="flex justify-center align-center w-full " >
+
+      <div
+        className="w-full bg-green-400 p-6"
+      
+      >
+
+
+
+      <h1  
+      >
+
+
+
+        hii there it's pranav
+      </h1>
+        </div>
+
+      <Buttons />
+    </div>
+  )
+}
+
+export default App
