a6e22b66f2646d8ba13fdaaae09c90515fc06953
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-19 16:08:46 +0530

    add React logic

--- passwordGeneratro/src/App.jsx
+++ passwordGeneratro/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   };
 
   const handleCopy = (e) => {
-    console.log(e.parentNode)
-  }
+    console.log(e.parentNode);
+  };
 
   //? To generate random password with memoization
   const passwordGenerator = useCallback(() => {
@@ -46,69 +46,87 @@ function App() {
 
   return (
     <>
-      <div className="container">
+      <div className=""
+      style={{
+        height : '500px',
+        width : '300px',
+        margin: '180px auto',
+        
 
+      }}
+      >
         <div>
 
-        
-        <input
-          type="text"
-          readOnly
-          value={password}
-          onClick={(e) => copyCode(e)}
-          style={{
-            textAlign: "center",
-            fontSize: "1.3rem",
-            backgroundColor: "#124",
-            border: "2px solid green",
-            borderRadius: "20px",
-            padding: '0.8rem'
-          }}
-        />
-        <button
+          {/*  first div  */}
+          <div className="">
+            <h1>Password generator</h1>
+
+            <input
+              type="text"
+              readOnly
+              value={password}
+              onClick={(e) => copyCode(e)}
+              style={{
+                textAlign: "center",
+                fontSize: "1.3rem",
+                backgroundColor: "#124",
+                border: "2px solid green",
+                borderRadius: "20px",
+                padding: "0.8rem 1.5rem ",
+              }}
+            />
+            {/* <button
         style={{
           textAlign: "center",
           fontSize: "1.3rem",
           backgroundColor: "#124",
           border: "2px solid green",
           borderRadius: "20px",
+          translate: "-2rem"
           
         }}
 
         onClick={(e)=>handleCopy(e)}
-        >Copy</button>
-        </div>
-      </div>
+        >Copy</button> */}
+          </div>
 
-      <div>
-        <div>
-          <input
-            name="len"
-            id="len"
-            type="range"
-            max={16}
-            min={7}
-            value={len}
-            onChange={(e) => setLen(e.target.value)}
-          />
-          <label htmlFor="len"> Length {len} </label>
-          <input
-            type="checkbox"
-            id="char"
-            name="char"
-            value={isChar}
-            onChange={(e) => setIsChar((prev) => !prev)}
-          />
 
-          <label htmlFor="char"> Character allowed </label>
+              {/*  second div  */}
+          <div>
+            <div>
+              <input
+                name="len"
+                id="len"
+                type="range"
+                max={16}
+                min={7}
+                value={len}
+                onChange={(e) => setLen(e.target.value)}
+              />
+              <label htmlFor="len"> Length {len} </label>
+              <input
+                type="checkbox"
+                id="char"
+                name="char"
+                value={isChar}
+                onChange={(e) => setIsChar((prev) => !prev)}
+              />
 
-          <input
-            type="checkbox"
-            id="num"
-            name="name"
-            value={isNumAllowed}
-            onChange={() => setIsNumAllowed((prev) => !prev)}
-          />
+              <label htmlFor="char"> Character </label>
+
+              <input
+                type="checkbox"
+                id="num"
+                name="name"
+                value={isNumAllowed}
+                onChange={() => setIsNumAllowed((prev) => !prev)}
+              />
+
+              <label htmlFor="num">Number</label>
+            </div>
+          </div>
+
+
         </div>
       </div>
     </>
