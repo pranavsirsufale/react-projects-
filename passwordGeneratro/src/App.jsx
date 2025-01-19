8ce9e6d96f1e60358a9ceb2eac3d8775a0b5c761
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-19 15:01:45 +0530

    add React logic

--- passwordGeneratro/src/App.jsx
+++ passwordGeneratro/src/App.jsx
@@ -3,12 +3,11 @@ import { useCallback, useEffect, useState } from "react";
 function App() {
   const [len, setLen] = useState(8);
   const [password, setPassword] = useState("");
+  const [isNumAllowed, setIsNumAllowed] = useState(false)
+  const [isChar ,setIsChar] = useState(false)
 
 
-  //? The string from the password to take 
-  let smtext = "abcdefghijklmnopqrstuvwxyz";
-  smtext += smtext.toUpperCase() + "~!@#~`$%^&*^()-_" + "0123456789";
-  const max = smtext.length - 1; 
+  
 
   //? to copy code on Clipboard
   const copyCode = (e) => {
@@ -19,33 +18,42 @@ function App() {
 
   //? To generate random password with memoization
   const passwordGenerator = useCallback(() => {
+
+    //? The string from the password to take 
+  let smtext = "abcdefghijklmnopqrstuvwxyz";
+  smtext += smtext.toUpperCase();
+  
+
+
+    if(isNumAllowed){
+      smtext += "0123456789";
+    }
+    if(isChar){
+      smtext += "~!@#~`$%^&*^()-_"
+    } 
+
+    
+    const max = smtext.length - 1; 
+
     let curr = "";
     for (let i = 0; i < len; i++) {
       curr += smtext[Math.round(Math.random() * max)];
     }
 
     setPassword(curr);
-  }, [len, password]);
+  }, [len,isChar,isNumAllowed]);
 
 
 
   useEffect(() => {
     passwordGenerator();
-  }, [len]);
+  }, [len,isChar,isNumAllowed]);
 
   return (
     <>
       <div className="container">
-        <div>
-          <input
-            name="len"
-            id="len"
-            type="range"
-            max={16}
-            min={7}
-            value={len}
-            onChange={(e) => setLen(e.target.value)}
-          />
+        
+         
           <input
             type="text"
             readOnly
@@ -59,10 +67,42 @@ function App() {
               borderRadius: "20px",
             }}
           />
+           <button>
+            Copy
+          </button>
+
+           
+
+
+
+
         </div>
 
         <div>
-          <label htmlFor="len"> {len} </label>
+        <div>
+          <input
+            name="len"
+            id="len"
+            type="range"
+            max={16}
+            min={7}
+            value={len}
+            onChange={(e) => setLen(e.target.value)}
+          />
+          <label htmlFor="len"> Length {len} </label>
+          <input type="checkbox" id="char" name="char" value={isChar} onChange={(e)=>setIsChar(prev => !prev)} />
+
+<label htmlFor="char"> Character allowed  </label>
+
+
+<input type="checkbox" 
+id="num"
+name="name"
+value={isNumAllowed}
+onChange={()=>setIsNumAllowed(prev=>!prev)}
+/>
+
+
         </div>
       </div>
     </>
