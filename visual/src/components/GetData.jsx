9e7a51c63042205eee0f6cec57d28fe4a011b3fa
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-25 11:19:21 +0530

    add React project

--- visual/src/components/GetData.jsx
+++ visual/src/components/GetData.jsx
@@ -1,7 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import Papa from 'papaparse';
+import SimpleCharts from './LinePlot';
+
+
+const CsvReader = () => {
+  const [data, setData] = useState([]);
+
+  console.log(data)
 
 
 
+// const newobje = {
+//     "":""
+// }
 
+// console.log(Object.is(newobje) === '' ? 'empty' : 'notempty')
+
+// console.log(newObj.length > 0 ? "true" : 'false')
+  
+
+
+
+  useEffect(() => {
+    fetch("../../public/csv/phd_gender.csv")
+      .then(response => response.text())
+      .then(csvText => {
+        Papa.parse(csvText, {
+          header: true,
+          complete: (result) => {
+            setData(result.data.filter((obje)=>obje[''] !== ''))
+          },
+          error: (error) => {
+            console.error('Error parsing CSV:', error);
+          }
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching CSV:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+     
 {
     data.length > 0 && <SimpleCharts data={data}  />
 }
+    </div>
+  );
+};
+
+export default CsvReader;
+
+
+
+
