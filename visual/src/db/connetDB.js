ee63a9db2efc41a15de9fb8b7076036db3b572ad
Pranav sirsufale <pranavdada334@gmail.com>
2025-01-30 08:34:26 +0530

    add React code

--- visual/src/db/connetDB.js
+++ visual/src/db/connetDB.js
@@ -3,7 +3,18 @@
 import mongoose from 'mongoose';
 
 const connectDB = () => {
-   
+    const uri = import.meta.env.VITE_MONGODB_URI
+    mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
 
 }
 
