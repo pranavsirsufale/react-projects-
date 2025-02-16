b99bfdf3084ef0b0faf35af9cf0659c70ab65d07
Pranav sirsufale <pranavdada334@gmail.com>
2025-02-16 11:07:16 +0530

    add react feature

--- contextapp/src/components/record/Voice.jsx
+++ contextapp/src/components/record/Voice.jsx
@@ -13,3 +13,29 @@
 // }
 
 // export default Voice
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AudioRecorder } from 'react-audio-voice-recorder';
+
+const addAudioElement = (blob) => {
+  const url = URL.createObjectURL(blob);
+  const audio = document.createElement("audio");
+  audio.src = url;
+  audio.controls = true;
+  document.body.appendChild(audio);
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+    <AudioRecorder 
+      onRecordingComplete={addAudioElement}
+      audioTrackConstraints={{
+        noiseSuppression: true,
+        echoCancellation: true,
+      }} 
+      downloadOnSavePress={true}
+      downloadFileExtension="webm"
+    />
+
+);
